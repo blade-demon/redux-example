dfead4e3221a27634601140f3f8b1d0ce2cd362f
xuziwei <xuziwei89@gmail.com>
2018-09-26 23:24:04 +0800

    First commit

---
+++ src/actions/postActions.js
@@ -0,0 +1,6 @@
+import { FETCH_POSTS } from "./types";
+export const fetchPosts = () => dispatch => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(posts => dispatch({ type: FETCH_POSTS, payload: posts }));
+};
